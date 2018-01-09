f2c827ec04e973a41cf526bfddcba5bf2096d652
Lav1234 <35216967+Lav1234@users.noreply.github.com>
2018-01-09 17:23:42 +0530

    Add files via upload
    Commit Lavanya

---
+++ cucumberTest/src/main/java/com/nisum/cucumberTest/App.java
@@ -0,0 +1,13 @@
+package com.nisum.cucumberTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ cucumberTest/src/test/java/com/nisum/cucumberTest/Featuresteps.java
@@ -0,0 +1,45 @@
+package com.nisum.cucumberTest;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Featuresteps {
+
+	@Given("^I visit the web site$")
+	public void i_visit_the_web_site() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^I enter username as \"([^\"]*)\"$")
+	public void i_enter_username_as(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^I enter password as \"([^\"]*)\"$")
+	public void i_enter_password_as(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^Login should fail$")
+	public void login_should_fail() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^Relogin option should be available$")
+	public void relogin_option_should_be_available() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^Relogin option should be not available$")
+	public void relogin_option_should_be_not_available() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+}
